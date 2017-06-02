e53ff923f4a7aa3b03cddbac2c1b2fea851385d1
jesus <mastermate@gmail.com>
2017-06-02 16:58:49 +0100

    initial commit for the compliance studio UI plugin

---
+++ ie.lero.compliance.ui/src/ie/lero/compliance/ui/views/ComplianceExplorerView.java
@@ -0,0 +1,22 @@
+package ie.lero.compliance.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ComplianceExplorerView extends ViewPart {
+
+	public static final String ID = "ie.lero.compliance.ui.complianceExplorerView";
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ie.lero.compliance.ui/src/ie/lero/compliance/ui/views/EnvironmentView.java
@@ -0,0 +1,22 @@
+package ie.lero.compliance.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class EnvironmentView extends ViewPart {
+
+	public static final String ID = "ie.lero.compliance.ui.environmentView";
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
